40d773868b7c8034ab147439c13257878ac28cd3
changyufei <changyufei@live.cn>
2022-11-08 23:19:20 +0800

    install django debug toolbox

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -78,7 +78,10 @@ class AccountViewSet(viewsets.ViewSet):
         查看用户当前登录状态和具体信息
         """
 
-        data = {'has_logged_in': request.user.is_authenticated}
+        data = {
+            'has_logged_in': request.user.is_authenticated,
+            'ip': request.META['REMOTE_ADDR'],
+        }
         if request.user.is_authenticated:
             data['user'] = UserSerializer(request.user).data
         return Response(data)
